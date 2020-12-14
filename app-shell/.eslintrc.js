7c0623c939cd121eda1f6557052f7cf6ae24a4e7
Andrei Zubov <reflection.dm@gmail.commit>
2020-12-14 11:37:21 +0100

    initial commit
    Add app-shell project and basic linting setup

---
+++ app-shell/.eslintrc.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+};
